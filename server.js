31047ddf9038c6ab6d3f8e2cc72edfef1816fb19
claire <cdersky@gmail.com>
2016-06-23 17:37:17 -0700

    Add Hello World
    Browser now displays a response at localhost:8080

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // basic server
 var http = require('http');
 
-var server = http.createServer();
+var server = http.createServer(function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('hello world');
+  response.end();
+});
+
 server.listen(8080);
