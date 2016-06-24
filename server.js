d85cbd465b4ac2c8f4736ab627c1e0e61335807c
claire <cdersky@gmail.com>
2016-06-23 17:48:35 -0700

    Add simple router
    to handle main route, /socket.html and errors

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 // basic server
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer(function(request, response){
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('hello world');
-  response.end();
+  var path = url.parse(request.url).pathname;
+
+  if(path === '/'){ // if on main page
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('hello world');
+    response.end();
+  } else if(path === '/socket.html'){
+    fs.readFile(__dirname + path, function(err, data){
+      if (err){
+        response.writeHead(404);
+        response.write('sorry, this doesnt exist');
+        response.end();        
+      } else {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data, "utf8");
+        response.end();
+      }
+    });   
+  } else {
+    response.writeHead(404);
+    response.write('sorry, this doesnt exist');
+    response.end(); 
+  }
+
 });
 
 server.listen(8080);
